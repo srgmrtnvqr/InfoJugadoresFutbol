218336da850a2a6cd546f5830ed9a211045cb016
srgmrtnvqr <srg.mrtn.vqr@gmail.com>
2024-08-17 22:45:15 +0200

    Mejora en la copia del pais del jugador y algunas excepciones.

--- datos_jugadores.py
+++ datos_jugadores.py
@@ -10,7 +10,7 @@ opciones = webdriver.ChromeOptions()
 opciones.add_experimental_option('excludeSwitches', ['enable-logging'])
 
 ##### LIGA ESPAÑOLA #####
-def obtenerEnlacesEquipos():    # Función que devuelve el enlace de cada equipo de La Liga
+def obtenerEnlacesEquiposLaLiga():    # Función que devuelve el enlace de cada equipo de La Liga
     url_principal = 'https://www.laliga.com/laliga-easports/clubes'
     peticion = requests.get(url_principal)  # Petición a la página web 
     time.sleep(2)
@@ -23,9 +23,9 @@ def obtenerEnlacesEquipos():    # Función que devuelve el enlace de cada equipo
         enlaces_equipos.append(enlace)
     return enlaces_equipos
 
-def obtenerEnlacesJugadores():  # Función que devuelve el enlace de cada jugador de La Liga
+def obtenerEnlacesJugadoresLaLiga():  # Función que devuelve el enlace de cada jugador de La Liga
     enlaces_jugadores = []
-    enlaces_equipos = obtenerEnlacesEquipos()
+    enlaces_equipos = obtenerEnlacesEquiposLaLiga()
     for i in range(len(enlaces_equipos)):
         url_equipo = 'https://www.laliga.com' + enlaces_equipos[i] + '/plantilla'
         peticion = requests.get(url_equipo)
@@ -38,7 +38,7 @@ def obtenerEnlacesJugadores():  # Función que devuelve el enlace de cada jugado
             enlaces_jugadores.append(enlace_jugador)
     return enlaces_jugadores
 
-def obtenerDatosJugadores(enlaces_jugadores):    # Función que devuelve un dataframe con los datos de todos los jugadores de La Liga
+def obtenerDatosJugadoresLaLiga():    # Función que devuelve un dataframe con los datos de todos los jugadores de La Liga
     nombres = []
     apodos = []
     fechas_nacimiento = []
@@ -49,59 +49,70 @@ def obtenerDatosJugadores(enlaces_jugadores):    # Función que devuelve un data
     equipos = []
     dorsales = []
     fotos = []
+    enlaces_jugadores = obtenerEnlacesJugadoresLaLiga()
     for i in range(len(enlaces_jugadores)):
         url_jugador = 'https://www.laliga.com' + enlaces_jugadores[i]
         peticion = requests.get(url_jugador)    # Petición a la página web
         time.sleep(2)
         sopa = BeautifulSoup(peticion.text, 'lxml') # Contenido de la web
         script_jugador = sopa.find_all('script', id='__NEXT_DATA__')
-        script_jugador = script_jugador[0].string   # Script con los datos del jugador
-        dicc_jugador = json.loads(script_jugador)   # Pasar la cadena de caracteres a un formato más legible
-        dicc_jugador = dicc_jugador['props']['pageProps']['player'] # Obtener el diccionario del jugador
-        lista_claves = list(dicc_jugador.keys())    # Lista con las claves del diccionario
-        posicion = dicc_jugador['squad']['position']['name']    
-        if posicion == 'Entrenador': # En principio no nos interesa la informacion de los entrenadores
-            pass
-        else:
-            if 'name' in lista_claves:
-                nombre = dicc_jugador['name']
-            else: 
-                nombre = ''
-            if 'nickname' in lista_claves:
-                apodo = dicc_jugador['nickname']
-            else:
-                apodo = ''
-            if 'date_of_birth' in lista_claves:
-                fecha_nacimiento = dicc_jugador['date_of_birth']
-            else: 
-                fecha_nacimiento = ''
-            if 'weight' in lista_claves:
-                peso = dicc_jugador['weight']
-            else:
-                peso = 0
-            if 'height' in lista_claves:
-                altura = dicc_jugador['height']
-            else: 
-                altura = 0
-            equipo = dicc_jugador['team']['name']   
-            pais = dicc_jugador['country']['id']
+        try:
+            script_jugador = script_jugador[0].string   # Script con los datos del jugador
+            dicc_jugador = json.loads(script_jugador)   # Pasar la cadena de caracteres a un formato más legible
+            dicc_jugador = dicc_jugador['props']['pageProps']['player'] # Obtener el diccionario del jugador
+            lista_claves = list(dicc_jugador.keys())    # Lista con las claves del diccionario
             try:
-                dorsal = dicc_jugador['squad']['shirt_number']
-            except: dorsal = ''
-            if 'photos' in lista_claves:
-                foto = (dicc_jugador['photos']['001']['256x278'])
+                posicion = dicc_jugador['squad']['position']['name']  
+            except:
+                posicion = ''
+            try:
+                pais = sopa.find('p', class_='styled__TextStyled-sc-1mby3k1-0 iBRpyN').get_text()   # Aquí está el nombre completo del país
+            except:
+                pais = ''  
+            if((posicion == 'Entrenador')|(posicion == 'Segundo entrenador')): # En principio no nos interesa la informacion de los entrenadores
+                pass
             else:
-                foto = ''
-            nombres.append(nombre)
-            apodos.append(apodo)
-            fechas_nacimiento.append(fecha_nacimiento)
-            pesos.append(peso)
-            alturas.append(altura)
-            paises.append(pais)
-            posiciones.append(posicion)
-            equipos.append(equipo)
-            dorsales.append(dorsal)
-            fotos.append(foto)  # Final del bucle FOR
+                if 'name' in lista_claves:
+                    nombre = dicc_jugador['name']
+                else: 
+                    nombre = ''
+                if 'nickname' in lista_claves:
+                    apodo = dicc_jugador['nickname']
+                else:
+                    apodo = ''
+                if 'date_of_birth' in lista_claves:
+                    fecha_nacimiento = dicc_jugador['date_of_birth']
+                else: 
+                    fecha_nacimiento = ''
+                if 'weight' in lista_claves:
+                    peso = dicc_jugador['weight']
+                else:
+                    peso = 0
+                if 'height' in lista_claves:
+                    altura = dicc_jugador['height']
+                else: 
+                    altura = 0
+                equipo = dicc_jugador['team']['name']
+                #pais = dicc_jugador['country']['id']   # Ahora este dato se obtiene antes
+                try:
+                    dorsal = dicc_jugador['squad']['shirt_number']
+                except: dorsal = ''
+                if 'photos' in lista_claves:
+                    foto = (dicc_jugador['photos']['001']['256x278'])
+                else:
+                    foto = ''
+                nombres.append(nombre)
+                apodos.append(apodo)
+                fechas_nacimiento.append(fecha_nacimiento)
+                pesos.append(peso)
+                alturas.append(altura)
+                paises.append(pais)
+                posiciones.append(posicion)
+                equipos.append(equipo)
+                dorsales.append(dorsal)
+                fotos.append(foto)  # Final del bucle FOR
+        except:
+            pass
     columnas = ['jugador', 'apodo', 'fecha', 'peso', 'altura', 'pais', 'posicion', 'equipo', 'dorsal', 'url']
     df_jugadores = pd.DataFrame([nombres, apodos, fechas_nacimiento, pesos, alturas, paises, 
                                 posiciones, equipos, dorsales, fotos], index=columnas)
